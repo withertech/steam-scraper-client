ff1736c99c0ae4c16d505a1a1f9c465cf4d9a424
Witherking25 <witherking@withertech.com>
2022-05-05 16:54:46 -0500

    temporarily neuter the dummy test i have

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,12 +1,12 @@
 import { ScrapeApi } from '../src/api'
 describe('api', () => {
 	describe('myPackage', () => {
-		it('should return a string containing the message', () => {
-			const api = new ScrapeApi("http://localhost:8080");
-			api.scrape("", "").then((result) => {
-				// expect(result).toMatch(message);
-				console.log(result);
-			})
-		});
+		// it('should return a string containing the message', () => {
+		// 	const api = new ScrapeApi("http://localhost:8080");
+		// 	api.scrape("", "").then((result) => {
+		// 		// expect(result).toMatch(message);
+		// 		console.log(result);
+		// 	})
+		// });
 	});
 });
