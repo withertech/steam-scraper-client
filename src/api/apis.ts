422fcb07d1a9664c4b42abb011c55f5bade7a93c
witherking25 <witherking@withertech.com>
2022-05-11 22:42:28 -0500

    feat(auth): add authentication

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -3,10 +3,14 @@ import { ScrapeApi } from './scrapeApi';
 import * as http from 'http';
 
 export class HttpError extends Error {
-    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
-        super('HTTP request failed');
-        this.name = 'HttpError';
-    }
+  constructor(
+    public response: http.IncomingMessage,
+    public body: any,
+    public statusCode?: number
+  ) {
+    super('HTTP request failed');
+    this.name = 'HttpError';
+  }
 }
 
 export { RequestFile } from '../model/models';
